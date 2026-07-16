9d889b13ccce717150f4c03aa2c244a3c428a0fd
claudiodeolli <claudio.dzuwa@gmail.com>
2026-07-15 21:30:12 -0300

    ajustes

--- next-app/src/app/(app)/plantao/page.js
+++ next-app/src/app/(app)/plantao/page.js
@@ -138,7 +138,7 @@ export default function PlantaoPage() {
                 alignItems: 'center',
                 justifyContent: 'center',
                 gap: '20px',
-                padding: '27px 24px',
+                padding: '27px 28px',
                 fontSize: '18px',
                 borderRadius: '100px',
                 fontWeight: '600',
